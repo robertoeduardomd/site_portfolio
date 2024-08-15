877034b94a4a67ffe0be66c7527b7122a2e3e3ca
Roberto Eduardo <robertoemd@gmail.com>
2024-08-15 03:06:11 -0300

    :hammer: atualizacao de portfolio modo escuro 2

--- public/mododark.js
+++ public/mododark.js
@@ -1,13 +1,23 @@
 const toggle = document.querySelector(".toggle-logo");
 const background = document.querySelector(".imgfundositeimg");
 const imggit = document.querySelector(".iconegithub");
+const experiencias = document.querySelector(".experiencias ");
+const labels = document.querySelectorAll(".conteudocolapsador ");
 
 toggle.addEventListener("change", () => {
   if (toggle.checked) {
     background.style.filter = "invert(100%)";
     imggit.style.filter = "invert(100%)";
+    experiencias.style.background = "black";
+    labels.forEach((label) => {
+      label.style.background = "rgb(233, 233, 233)";
+    });
   } else {
     background.style.filter = "invert(0%)";
     imggit.style.filter = "invert(0%)";
+    experiencias.style.background = "rgb(233, 233, 233)";
+    labels.forEach((label) => {
+      label.style.background = "rgb(233, 233, 233)";
+    });
   }
 });
